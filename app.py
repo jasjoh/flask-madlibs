0e929a029915387cbda8dccc6cf987025f314536
Jason Johnson <jasjoh@live.com>
2023-07-19 06:31:05 -0700

    added initial view functions; updated questions.html to use jinja loops; questions now good to go

--- app.py
+++ app.py
@@ -7,3 +7,12 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get('/questions')
+def questions():
+    prompts = silly_story.prompts # list a of strings (words)
+    return render_template("questions.html", prompts=prompts)
+
+@app.get('/results')
+def results():
+    return render_template("results.html")
