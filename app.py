94d548e2afd6eb79b626a15bcdebfe603c9b1120
Tichin Wu <twu38@mail.ccsf.edu>
2023-07-19 16:32:28 -0700

    finish first part

--- app.py
+++ app.py
@@ -8,11 +8,19 @@ app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
+
 @app.get('/questions')
-def questions():
-    prompts = silly_story.prompts # list a of strings (words)
+def questions():  # ask_questions
+    """display the form which prompts for values """
+
+    prompts = silly_story.prompts  # list a of strings (words)
     return render_template("questions.html", prompts=prompts)
 
+
 @app.get('/results')
-def results():
-    return render_template("results.html")
+def results():  # show_result
+    """Turns the values to story and displays it"""
+    # request.args is a already dict like obj
+    answers = {key: request.args[key] for key in request.args}
+    story = silly_story.get_result_text(answers)
+    return render_template("results.html", story=story)
